1ddca1611bc2bfab52cfc9e26657cf3290ca44dc
dennx2 <jdcht1228@gmail.com>
2023-11-04 16:05:03 -0400

    added user register/login/logout

---
+++ backend/website/models/users.py
@@ -0,0 +1,11 @@
+from .. import db
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    role = db.Column(db.String(10))
+    password = db.Column(db.String(80))
+    date_created = db.Column(db.DateTime, default = datetime.now())
---
+++ backend/website/routes/auth.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, redirect, url_for, request, jsonify
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..models.users import User
+from .. import db
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.app_context_processor
+def inject_user():
+    return dict(user=current_user)
+
+
+@auth.post("/login")
+def login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
+        # If authentication is successful
+        login_user(user, remember=True)
+        print("Log in success!")
+        return jsonify({'message': 'Login successful'})
+    else:
+        return jsonify({'error': 'Invalid username or password'}), 401
+
+
+@auth.post("/sign-up")
+def sign_up():
+    data = request.get_json()
+    print(data)
+
+    username = data["username"]
+    email = data["email"]
+    password1 = data["password1"]
+    password2 = data["password2"]
+
+    email_exists = User.query.filter_by(email=email).first()
+    username_exists = User.query.filter_by(username=username).first()
+    if email_exists:
+        return jsonify({'error': 'Email or Username is already in use'}), 400
+    elif username_exists:
+        return jsonify({'error': 'Email or Username is already in use'}), 400
+    elif password1 != password2:
+        return jsonify({'error': "Passwords don't match"}), 400
+    elif len(username) < 3:
+        return jsonify({'error': 'Username must be at least 3 characters long'}), 400
+    elif len(password1) < 6:
+        return jsonify({'error': 'Password must be at least 6 characters long'}), 400
+    else:
+        new_user = User(username=username, email=email, password=generate_password_hash(
+            password1, method="scrypt"))
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user, remember=True)
+        print("Sign up success!")
+        return jsonify({'message': 'Signup successful'})
+
+
+@login_required
+@auth.route("/logout")
+def logout():
+    # logout_user to clear the user's session
+    logout_user()
+    print("Log out success!")
+    return jsonify({'message': 'Logout successful'})
