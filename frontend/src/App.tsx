5df95784b853dbce667c9c21c11012e0007f00e3
GrayChiang <graychiang@bluetechnology.com.tw>
2023-11-04 22:47:08 -0400

    loginpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // article
+  // const [articles, setArticles] = useState([]);
+  // Modify the current state by setting the new data to
+  // the response from the backend
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "john_doe",
+        password: "mysecretpassword",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      // .then((response) => setArticles(response))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
@@ -28,8 +48,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>TestAPI</div>
+      <div className="App container m-4">
+        <div className="row">
+          <div className="text-center">
+            <h1>Connecting a React Frontend to a Flask Backend.</h1>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
